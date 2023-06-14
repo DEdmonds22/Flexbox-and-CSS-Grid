db938b208f8afa744d9a23128644afcdc19400f6
DEdmonds22 <ddedmonds22@gmail.com>
2023-06-14 14:31:30 -0400

    Your First CSS Grid. Corrected JS link in index.html.

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const asideEl = document.querySelector("aside");
+const mainEl = document.querySelector("main");
+const footerEl = document.querySelector("footer");
+
+asideEl.classList.add("flex-ctr");  /* applies prev. defined class flex-ctr to the main element */
+mainEl.classList.add("flex-ctr");
+footerEl.classList.add("flex-ctr");
